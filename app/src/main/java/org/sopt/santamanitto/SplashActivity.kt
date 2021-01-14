c15a149b4dcbde9594eb6850b322552cedbf5f64
KIM SEONGGYU <link5658@gmail.com>
2021-01-14 13:36:49 +0900

    feat: Add SplashActivity

---
+++ app/src/main/java/org/sopt/santamanitto/SplashActivity.kt
@@ -0,0 +1,28 @@
+package org.sopt.santamanitto
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.DataBindingUtil.*
+import org.sopt.santamanitto.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    companion object {
+        private const val SPLASH_DELAY = 1000L
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView<ActivitySplashBinding>(this, R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, SPLASH_DELAY)
+
+    }
+}
