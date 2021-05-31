5768e810b9024f1ae19d97512cd69f9e4c7127c7
KIM SEONGGYU <link5658@gmail.com>
2021-05-31 17:44:08 +0900

    refactor: Merge ResultFragment with FinishFragment

--- app/src/main/java/org/sopt/santamanitto/room/manittoroom/fragment/FinishFragment.kt
+++ app/src/main/java/org/sopt/santamanitto/room/manittoroom/fragment/FinishFragment.kt
@@ -3,22 +3,39 @@ package org.sopt.santamanitto.room.manittoroom.fragment
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.*
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import org.sopt.santamanitto.R
 import org.sopt.santamanitto.databinding.FragmentManittoRoomFinishBinding
+import org.sopt.santamanitto.databinding.LayoutFinishBinding
+import org.sopt.santamanitto.databinding.LayoutResultBinding
 import org.sopt.santamanitto.room.manittoroom.ManittoRoomViewModel
+import org.sopt.santamanitto.room.manittoroom.ResultAdapter
 import org.sopt.santamanitto.view.setLayoutHeight
+import javax.inject.Inject
+import android.view.View.OnLayoutChangeListener as OnLayoutChangeListener1
 
 @AndroidEntryPoint
 class FinishFragment: Fragment() {
 
+    companion object {
+        private const val SCREEN_KEY = "isFinishScreen"
+    }
+
     private lateinit var binding: FragmentManittoRoomFinishBinding
 
+    private lateinit var finishBinding: LayoutFinishBinding
+    private lateinit var resultBinding: LayoutResultBinding
+
     private val manittoRoomViewModel: ManittoRoomViewModel by activityViewModels()
+
+    @Inject
+    lateinit var resultAdapter: ResultAdapter
+
+    private var isFinishScreen = true
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,18 +43,29 @@ class FinishFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentManittoRoomFinishBinding.inflate(inflater, container, false)
-            .apply {
-                lifecycleOwner = viewLifecycleOwner
-                viewModel = manittoRoomViewModel
-                root.addOnLayoutChangeListener(object : View.OnLayoutChangeListener {
-                    override fun onLayoutChange(v: View, left: Int, top: Int, right: Int, bottom: Int, oldLeft: Int,
-                                                oldTop: Int, oldRight: Int, oldBottom: Int) {
-                        binding.root.removeOnLayoutChangeListener(this)
+        return binding.root
+    }
 
-                        initMissionText()
-                    }
-                })
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.run {
+            lifecycleOwner = viewLifecycleOwner
+            viewModel = manittoRoomViewModel
+            root.addOnLayoutChangeListener(object : OnLayoutChangeListener1 {
+                override fun onLayoutChange(v: View, left: Int, top: Int, right: Int, bottom: Int, oldLeft: Int,
+                                            oldTop: Int, oldRight: Int, oldBottom: Int) {
+                    binding.root.removeOnLayoutChangeListener(this)
+
+                    initMissionText()
+                }
+            })
+        }
+
+        finishBinding = binding.includeFinishMatched.apply {
+            viewModel = manittoRoomViewModel
+            lifecycleOwner = viewLifecycleOwner
+        }
+
+        resultBinding = binding.includeFinishResult
 
         manittoRoomViewModel.run {
             refreshManittoRoomInfo()
@@ -45,14 +73,25 @@ class FinishFragment: Fragment() {
         }
 
         setOnClickListener()
+    }
 
-        return binding.root
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+        val value = savedInstanceState?.getBoolean(SCREEN_KEY, true)?: true
+        if (!value) {
+            showResultView()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putBoolean(SCREEN_KEY, isFinishScreen)
+        super.onSaveInstanceState(outState)
     }
 
     private fun setOnClickListener() {
         binding.run {
             santabottombuttonFinish.setOnClickListener {
-                navigateResultFragment()
+                showResultView()
             }
             santabackgroundFinish.setOnBackKeyClickListener {
                 requireActivity().finish()
@@ -60,12 +99,34 @@ class FinishFragment: Fragment() {
         }
     }
 
-    private fun navigateResultFragment() {
-        findNavController().navigate(FinishFragmentDirections.actionFinishFragmentToResultFragment())
+    private fun showResultView() {
+        isFinishScreen = false
+        //finish 뷰 가리기, result 뷰 세팅
+        finishBinding.root.visibility = GONE
+        resultBinding.run {
+            viewModel = manittoRoomViewModel
+            lifecycleOwner = viewLifecycleOwner
+            root.visibility = VISIBLE
+            recyclerviewResult.adapter = resultAdapter
+        }
+
+        //총 인원 설정
+        manittoRoomViewModel.members.observe(viewLifecycleOwner) {
+            resultBinding.textviewResultTitle.text = String.format(getString(R.string.result_title), it.size)
+        }
+
+        //버튼 동작 변경
+        binding.santabottombuttonFinish.run {
+            setText(R.string.result_bottom)
+            setOnClickListener {
+                requireActivity().finish()
+            }
+        }
     }
 
     private fun initMissionText() {
-        binding.textviewFinishMission.run {
+        finishBinding.textviewFinishMission.run {
+            //현재 폰트의 2줄 크기를 반영하기 위함. 기본 텍스트가 \n\n 임
             setLayoutHeight(this, this.height)
             manittoRoomViewModel.missionToMe.observe(viewLifecycleOwner) {
                 text = if (it.isNullOrEmpty()) {
