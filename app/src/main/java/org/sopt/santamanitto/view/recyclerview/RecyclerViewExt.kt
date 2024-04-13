e5db818d632abea20b19c7a356c6c4d380da3f0e
HaeTi <t9h9@naver.com>
2024-04-14 01:52:22 +0900

    [QA/#133] replaceAll 바인딩어뎁터에 분기처리 로직 추가

--- app/src/main/java/org/sopt/santamanitto/view/recyclerview/RecyclerViewExt.kt
+++ app/src/main/java/org/sopt/santamanitto/view/recyclerview/RecyclerViewExt.kt
@@ -3,23 +3,25 @@ package org.sopt.santamanitto.view.recyclerview
 import android.view.View
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 
 @BindingAdapter("replaceAll")
-fun RecyclerView.replaceAll(list: List<Nothing>?) {
-    if (adapter != null) {
-        (adapter as BaseAdapter<*>).run {
-            if (list != null) {
-                visibility = if (list.isEmpty()) {
-                    View.INVISIBLE
-                } else {
-                    View.VISIBLE
-                }
-                setList(list)
+fun <T> RecyclerView.replaceAll(list: List<T>?) {
+    (this.adapter as? ListAdapter<T, *>)?.let { adapter ->
+        adapter.submitList(list?.toList())
+        this.visibility = if (list.isNullOrEmpty()) View.INVISIBLE else View.VISIBLE
+    } ?: (this.adapter as? BaseAdapter<T>)?.run {
+        if (list != null) {
+            setList(list)
+            this@replaceAll.visibility = if (list.isEmpty()) {
+                View.INVISIBLE
             } else {
-                clear()
-                visibility = View.INVISIBLE
+                View.VISIBLE
             }
+        } else {
+            clear()
+            this@replaceAll.visibility = View.INVISIBLE
         }
     }
 }
