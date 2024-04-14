d54a91834961288be1aec2eac26e96a260c9650e
HaeTi <t9h9@naver.com>
2024-04-15 00:20:01 +0900

    [QA/#141] firebase remote config에서 값 받아와서 점검 시 dialog 노출

--- app/src/main/java/org/sopt/santamanitto/SplashViewModel.kt
+++ app/src/main/java/org/sopt/santamanitto/SplashViewModel.kt
@@ -1,17 +1,23 @@
 package org.sopt.santamanitto
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import org.sopt.santamanitto.update.version.Version
 import org.sopt.santamanitto.update.version.VersionChecker
 import org.sopt.santamanitto.user.data.UserLoginModel
 import org.sopt.santamanitto.user.data.controller.UserController
 import org.sopt.santamanitto.user.data.source.UserMetadataSource
+import timber.log.Timber
 import javax.inject.Inject
 import javax.inject.Named
 
@@ -36,12 +42,24 @@ class SplashViewModel @Inject constructor(
     val loginSuccess: LiveData<LoginState>
         get() = _loginSuccess
 
+    private val _remoteServerCheck = MutableStateFlow(false)
+    val remoteServerCheck: StateFlow<Boolean>
+        get() = _remoteServerCheck
+
+    private val _remoteServerCheckMessage = MutableStateFlow("")
+    val remoteServerCheckMessage: StateFlow<String>
+        get() = _remoteServerCheckMessage
+
+    init {
+        fetchRemoteConfig()
+    }
+
     fun checkUpdate() {
         viewModelScope.launch {
             try {
                 _latestVersion.value = versionChecker.getLatestVersion()
             } catch (e: Exception) {
-                Log.e(this.javaClass.simpleName, e.stackTraceToString())
+                Timber.tag(this.javaClass.simpleName).e(e.stackTraceToString())
                 _versionCheckFail.value = true
             }
         }
@@ -69,6 +87,34 @@ class SplashViewModel @Inject constructor(
             }
         })
     }
+
+    private fun fetchRemoteConfig() {
+        getRemoteConfigInstance().fetchAndActivate().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                _remoteServerCheck.value = getRemoteConfigInstance().getBoolean("test_server_check")
+                _remoteServerCheckMessage.value =
+                    getRemoteConfigInstance().getString("server_check_message")
+            } else {
+                // TODO : Firebase Remote Config fetch 실패 시 로직 추가
+                Timber.tag("RemoteConfig").e("Fetch Failed")
+            }
+        }
+    }
+
+    companion object {
+        private val remoteConfig by lazy {
+            Firebase.remoteConfig.apply {
+                setConfigSettingsAsync(
+                    remoteConfigSettings {
+                        minimumFetchIntervalInSeconds = 0
+                    }
+                )
+            }
+        }
+
+        fun getRemoteConfigInstance(): FirebaseRemoteConfig = remoteConfig
+    }
+
 
     enum class LoginState {
         SUCCESS, FAIL, WAITING, ERROR
