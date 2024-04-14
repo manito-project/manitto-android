68344700662730ec357df86ddbf1ff8cd38e715c
HaeTi <t9h9@naver.com>
2024-04-15 00:20:01 +0900

    [QA/#141] firebase remote config에서 값 받아와서 점검 시 dialog 노출

--- app/src/main/java/org/sopt/santamanitto/SplashActivity.kt
+++ app/src/main/java/org/sopt/santamanitto/SplashActivity.kt
@@ -9,7 +9,9 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.databinding.DataBindingUtil.setContentView
+import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 import org.sopt.santamanitto.databinding.ActivitySplashBinding
 import org.sopt.santamanitto.main.MainActivity
 import org.sopt.santamanitto.update.version.Version
@@ -26,6 +28,7 @@ class SplashActivity : AppCompatActivity() {
     private val splashViewModel: SplashViewModel by viewModels()
 
     private var isDelayDone = false
+    private var isDialogShown = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,55 +36,87 @@ class SplashActivity : AppCompatActivity() {
         setContentView<ActivitySplashBinding>(this, R.layout.activity_splash)
 
         splashViewModel.run {
-            latestVersion.observe(this@SplashActivity) { latestVersion ->
-                if (latestVersion == null) {
-                    return@observe
-                }
-                if (latestVersion.compare(BuildConfig.VERSION_NAME, Version.MAJOR) > 0) {
-                    showUpdateDialog()
-                } else {
-                    tryLogin()
+            lifecycleScope.launch {
+                remoteServerCheck.collect { isRemoteServerCheck ->
+                    if (isRemoteServerCheck && !isDialogShown) {
+                        remoteServerCheckMessage.collect { message ->
+                            val formattedMessage = message.replace("\\n", "\n")
+                            val dialog = RoundDialogBuilder()
+                                .setContentText(formattedMessage)
+                                .addHorizontalButton(getString(R.string.splash_network_error_dialog_button)) {
+                                    finish()
+                                }
+                                .build()
+
+                            // commitAllowingStateLoss()를 사용하여 다이얼로그 표시
+                            if (!supportFragmentManager.isStateSaved) {
+                                dialog.show(supportFragmentManager, "error")
+                            } else {
+                                supportFragmentManager.beginTransaction().add(dialog, "error")
+                                    .commitAllowingStateLoss()
+                            }
+                            isDialogShown = true
+                        }
+                    } else {
+                        observeData()
+                    }
                 }
             }
-
-            versionCheckFail.observe(this@SplashActivity) {
-                if (it) {
-                    // 버전 체크에 실패하더라도 로그인 시도는 해본다. (API 안정성이 보장되지 않음)
-                    tryLogin()
-                }
-            }
-
-            loginSuccess.observe(this@SplashActivity) {
-                startNextActivity()
-            }
-
-            checkUpdate()
         }
+    }
+
+    private fun SplashViewModel.observeData() {
+        latestVersion.observe(this@SplashActivity) { latestVersion ->
+            if (isDialogShown) {
+                return@observe
+            } else if (latestVersion == null) {
+                return@observe
+            } else if (latestVersion.compare(BuildConfig.VERSION_NAME, Version.MAJOR) > 0) {
+                showUpdateDialog()
+            } else {
+                tryLogin()
+            }
+        }
+
+        versionCheckFail.observe(this@SplashActivity) {
+            if (it && !isDialogShown) {
+                // 버전 체크에 실패하더라도 로그인 시도는 해본다. (API 안정성이 보장되지 않음)
+                tryLogin()
+            }
+        }
+
+        loginSuccess.observe(this@SplashActivity) {
+            if (!isDialogShown) startNextActivity()
+        }
+
+        checkUpdate()
     }
 
     private fun showUpdateDialog() {
         RoundDialogBuilder()
-                .setContentText(getString(R.string.update_dialog_content))
-                .addHorizontalButton(getString(R.string.update_dialog_exit)) {
-                    finish()
-                }
-                .addHorizontalButton(getString(R.string.update_dialog_update)) {
-                    goToStore()
-                }
-                .enableCancel(false)
-                .build()
-                .show(supportFragmentManager, "update")
+            .setContentText(getString(R.string.update_dialog_content))
+            .addHorizontalButton(getString(R.string.update_dialog_exit)) {
+                finish()
+            }
+            .addHorizontalButton(getString(R.string.update_dialog_update)) {
+                goToStore()
+            }
+            .enableCancel(false)
+            .build()
+            .show(supportFragmentManager, "update")
     }
 
     private fun goToStore() {
         val intent = Intent(Intent.ACTION_VIEW).apply {
             data = Uri.parse(
-                    "https://play.google.com/store/apps/details?id=${BuildConfig.APPLICATION_ID}")
+                "https://play.google.com/store/apps/details?id=${BuildConfig.APPLICATION_ID}"
+            )
             setPackage("com.android.vending")
         }
         startActivity(intent)
         finish()
     }
+
 
     private fun tryLogin() {
         Handler(Looper.getMainLooper()).postDelayed({
@@ -94,24 +129,26 @@ class SplashActivity : AppCompatActivity() {
 
     private fun startNextActivity() {
         val loginState = splashViewModel.loginSuccess.value
-        if (isDelayDone && loginState != SplashViewModel.LoginState.WAITING) {
+        if (isDelayDone && loginState != SplashViewModel.LoginState.WAITING && !isDialogShown) {
             when (loginState) {
                 SplashViewModel.LoginState.SUCCESS -> {
                     startActivity(Intent(this@SplashActivity, MainActivity::class.java))
                     finish()
                 }
+
                 SplashViewModel.LoginState.FAIL -> {
                     startActivity(Intent(this@SplashActivity, SignInActivity::class.java))
                     finish()
                 }
+
                 else -> {
                     RoundDialogBuilder()
                         .setContentText(getString(R.string.splash_network_error_dialog), false)
                         .addHorizontalButton(getString(R.string.splash_network_error_dialog_button)) {
-                                finish()
-                            }
-                            .build()
-                            .show(supportFragmentManager, "error")
+                            finish()
+                        }
+                        .build()
+                        .show(supportFragmentManager, "error")
                 }
             }
         }
--- app/src/main/java/org/sopt/santamanitto/user/data/controller/RetrofitUserController.kt
+++ app/src/main/java/org/sopt/santamanitto/user/data/controller/RetrofitUserController.kt
@@ -9,9 +9,9 @@ import org.sopt.santamanitto.user.network.UserService
 import retrofit2.Call
 import retrofit2.Callback
 
-class RetrofitUserController(private val userService: UserService): UserController {
+class RetrofitUserController(private val userService: UserService) : UserController {
     override fun login(serialNumber: String, callback: UserController.LoginCallback) {
-        userService.login(serialNumber).enqueue(object: Callback<Response<UserCheckModel>> {
+        userService.login(serialNumber).enqueue(object : Callback<Response<UserCheckModel>> {
             override fun onResponse(
                 call: Call<Response<UserCheckModel>>,
                 response: retrofit2.Response<Response<UserCheckModel>>
@@ -21,7 +21,14 @@ class RetrofitUserController(private val userService: UserService): UserControll
                         if (response.body()!!.message == "해당 시리얼 넘버를 가진 유저가 있습니다") {
                             val result = response.body()!!.data.user
                             val accessToken = response.body()!!.data.accessToken
-                            callback.onLoginSuccess(UserLoginModel(result.userName, result.serialNumber, result.id, accessToken))
+                            callback.onLoginSuccess(
+                                UserLoginModel(
+                                    result.userName,
+                                    result.serialNumber,
+                                    result.id,
+                                    accessToken
+                                )
+                            )
                         } else {
                             callback.onLoginFailed(false)
                         }
@@ -39,9 +46,13 @@ class RetrofitUserController(private val userService: UserService): UserControll
         })
     }
 
-    override fun createAccount(userName: String, serialNumber: String, callback: UserController.CreateAccountCallback) {
-        userService.createAccount(UserLoginModel(userName, serialNumber)).start(object:
-                RequestCallback<UserLoginModel> {
+    override fun createAccount(
+        userName: String,
+        serialNumber: String,
+        callback: UserController.CreateAccountCallback
+    ) {
+        userService.createAccount(UserLoginModel(userName, serialNumber)).start(object :
+            RequestCallback<UserLoginModel> {
             override fun onSuccess(data: UserLoginModel) {
                 callback.onCreateAccountSuccess(data)
             }
