4379e5847b6baf816e3b0f2c9ed9accbd98c69d7
Sangho Kim <marchbreeze03@gmail.com>
2024-04-14 15:45:23 +0900

    [FIX/#138] 매칭 대기 뷰 ListAapter 로 수정

--- app/src/main/java/org/sopt/santamanitto/room/manittoroom/MemberAdapter.kt
+++ app/src/main/java/org/sopt/santamanitto/room/manittoroom/MemberAdapter.kt
@@ -1,16 +1,25 @@
 package org.sopt.santamanitto.room.manittoroom
 
 import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
 import org.sopt.santamanitto.room.manittoroom.network.ManittoRoomMember
-import org.sopt.santamanitto.view.recyclerview.BaseAdapter
-import org.sopt.santamanitto.view.recyclerview.BaseViewHolder
+import org.sopt.santamanitto.util.ItemDiffCallback
 
-class MemberAdapter: BaseAdapter<ManittoRoomMember>() {
+class MemberAdapter : ListAdapter<ManittoRoomMember, MemberViewHolder>(DiffUtil) {
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
-    ): BaseViewHolder<ManittoRoomMember, *> {
-        return MemberViewHolder(parent)
+    ): MemberViewHolder = MemberViewHolder(parent)
+
+    override fun onBindViewHolder(holder: MemberViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    companion object {
+        private val DiffUtil = ItemDiffCallback<ManittoRoomMember>(
+            onContentsTheSame = { oldItem, newItem -> oldItem.userId == newItem.userId },
+            onItemsTheSame = { oldItem, newItem -> oldItem == newItem }
+        )
     }
 }
