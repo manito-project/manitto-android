32749b008dd1b0da2cec1a67e6906d74acb1fc6d
Sangho Kim <marchbreeze03@gmail.com>
2024-10-03 10:20:32 +0900

    [FIX/#168] 피리어드피커 최대치 대응

--- app/src/main/java/org/sopt/santamanitto/view/SantaEditText.kt
+++ app/src/main/java/org/sopt/santamanitto/view/SantaEditText.kt
@@ -12,203 +12,216 @@ import androidx.databinding.DataBindingUtil
 import org.sopt.santamanitto.R
 import org.sopt.santamanitto.databinding.SantaEditTextBinding
 
-class SantaEditText @JvmOverloads
-constructor(
-    context: Context,
-    attrs: AttributeSet? = null,
-    defStyleAttr: Int = 0
-) : LinearLayout(context, attrs, defStyleAttr), TextObservable {
-
-    companion object {
-        private const val BUTTON_NONE = 0
-        private const val BUTTON_ADD = 1
-        private const val BUTTON_DELETE = 2
-    }
-
-    private var binding: SantaEditTextBinding = DataBindingUtil.inflate(
-        LayoutInflater.from(context),
-        R.layout.santa_edit_text,
-        this,
-        true
-    )
-
-    private val editText = binding.edittextSantaedittext
-
-    private val preview = binding.textviewSantaedittextPreview
-
-    private var rightButton = binding.santasmallbuttonSantaedittext
-
-    private var buttonStyle = BUTTON_NONE
-
-    private var addListener: ((String) -> Unit)? = null
-
-    var hint: String?
-        get() = editText.hint.toString()
-        set(value) {
-            editText.hint = value
+class SantaEditText
+    @JvmOverloads
+    constructor(
+        context: Context,
+        attrs: AttributeSet? = null,
+        defStyleAttr: Int = 0,
+    ) : LinearLayout(context, attrs, defStyleAttr),
+        TextObservable {
+        companion object {
+            private const val BUTTON_NONE = 0
+            private const val BUTTON_ADD = 1
+            private const val BUTTON_DELETE = 2
         }
 
-    override var text: String?
-        get() = editText.text.toString()
-        set(value) {
-            editText.setText(value)
-        }
+        private var binding: SantaEditTextBinding =
+            DataBindingUtil.inflate(
+                LayoutInflater.from(context),
+                R.layout.santa_edit_text,
+                this,
+                true,
+            )
 
-    var isEditable = true
-        set(value) {
-            editText.isEnabled = value
-            field = value
-        }
+        private val editText = binding.edittextSantaedittext
 
-    init {
-        val typeArray = context.theme.obtainStyledAttributes(
-            attrs,
-            R.styleable.SantaEditText,
-            0, 0
-        )
+        private val preview = binding.textviewSantaedittextPreview
 
-        if (typeArray.hasValue(R.styleable.SantaEditText_rightButton)) {
-            buttonStyle = typeArray.getInt(R.styleable.SantaEditText_rightButton, BUTTON_NONE)
-        }
+        private var rightButton = binding.santasmallbuttonSantaedittext
 
-        if (typeArray.hasValue(R.styleable.SantaEditText_hint)) {
-            hint = typeArray.getString(R.styleable.SantaEditText_hint)
-        }
+        private var buttonStyle = BUTTON_NONE
 
-        if (typeArray.hasValue(R.styleable.SantaEditText_isSingleLine)) {
-            editText.isSingleLine =
-                typeArray.getBoolean(R.styleable.SantaEditText_isSingleLine, true)
-        }
+        private var addListener: ((String) -> Unit)? = null
 
-        if (typeArray.hasValue(R.styleable.SantaEditText_isEditable)) {
-            isEditable = typeArray.getBoolean(R.styleable.SantaEditText_isEditable, true)
-        }
-
-        if (typeArray.hasValue(R.styleable.SantaEditText_isSlim)) {
-            val isSlim = typeArray.getBoolean(R.styleable.SantaEditText_isSlim, false)
-            if (isSlim) {
-
+        var hint: String?
+            get() = editText.hint.toString()
+            set(value) {
+                editText.hint = value
             }
-        }
 
-        if (typeArray.hasValue(R.styleable.SantaEditText_maxLength)) {
-            val maxLength = typeArray.getInt(R.styleable.SantaEditText_maxLength, -1)
-            if (maxLength != -1) {
-                setMaxLength(maxLength)
+        override var text: String?
+            get() = editText.text.toString()
+            set(value) {
+                editText.setText(value)
             }
-        }
 
-        if (typeArray.hasValue(R.styleable.SantaEditText_maxLines)) {
-            val maxLines = typeArray.getInt(R.styleable.SantaEditText_maxLines, -1)
-            if (maxLines != -1) {
-                setMaxLines(maxLines)
+        var isEditable = true
+            set(value) {
+                editText.isEnabled = value
+                field = value
             }
-        }
 
-        typeArray.recycle()
+        init {
+            val typeArray =
+                context.theme.obtainStyledAttributes(
+                    attrs,
+                    R.styleable.SantaEditText,
+                    0,
+                    0,
+                )
 
-        when (buttonStyle) {
-            BUTTON_NONE -> rightButton.visibility = View.INVISIBLE
-            BUTTON_ADD -> setAddImage()
-            BUTTON_DELETE -> setDeleteImage()
-        }
-
-        rightButton.setOnClickListener {
-            if (text.isNullOrBlank()) {
-                return@setOnClickListener
+            if (typeArray.hasValue(R.styleable.SantaEditText_rightButton)) {
+                buttonStyle = typeArray.getInt(R.styleable.SantaEditText_rightButton, BUTTON_NONE)
             }
-            addListener?.invoke(text!!)
-        }
-    }
 
-    fun setButtonClickListener(listener: (String) -> Unit) {
-        addListener = listener
-    }
-
-    fun setAddImage() {
-        rightButton.run {
-            rightButton.visibility = View.VISIBLE
-            setPlusImage()
-        }
-        buttonStyle = BUTTON_ADD
-    }
-
-    fun setDeleteImage() {
-        rightButton.run {
-            rightButton.visibility = View.VISIBLE
-            setCancelImage()
-        }
-        buttonStyle = BUTTON_DELETE
-    }
-
-    private fun setMaxLength(maxLength: Int) {
-        editText.filters = arrayOf<InputFilter>(InputFilter.LengthFilter(maxLength))
-    }
-
-    private fun setMaxLines(maxLines: Int) {
-        editText.maxLines = maxLines
-    }
-
-    fun setContentWidth(width: Int) {
-        editText.layoutParams.width = width
-        preview.layoutParams.width = width
-    }
-
-    override fun setOnKeyListener(l: OnKeyListener) {
-        editText.setOnKeyListener(l)
-    }
-
-    override fun addTextChangeListener(textWatcher: TextWatcher) {
-        editText.addTextChangedListener(textWatcher)
-    }
-
-    fun compress(isCompress: Boolean) {
-        if (isCompress) {
-            preview.text = text
-            editText.visibility = View.GONE
-        } else {
-            preview.text = null
-            editText.visibility = View.VISIBLE
-        }
-    }
-
-    fun setSelection(index: Int) {
-        editText.setSelection(index)
-    }
-
-    class SantaEditLimitLengthWatcher(
-        private val nameInput: SantaEditText,
-        private val maxLength: Int,
-        private val onWarning: (isWarning: Boolean) -> Unit
-    ) : TextWatcher {
-
-        private var isWarning = false
-        private var preText = ""
-        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            preText = s.toString()
-        }
-
-        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            if (s.isNullOrEmpty() || s == preText) {
-                return
+            if (typeArray.hasValue(R.styleable.SantaEditText_hint)) {
+                hint = typeArray.getString(R.styleable.SantaEditText_hint)
             }
-            if (s.length > maxLength) {
-                nameInput.run {
-                    text = s.substring(0, maxLength)
-                    setSelection(maxLength)
+
+            if (typeArray.hasValue(R.styleable.SantaEditText_isSingleLine)) {
+                editText.isSingleLine =
+                    typeArray.getBoolean(R.styleable.SantaEditText_isSingleLine, true)
+            }
+
+            if (typeArray.hasValue(R.styleable.SantaEditText_isEditable)) {
+                isEditable = typeArray.getBoolean(R.styleable.SantaEditText_isEditable, true)
+            }
+
+            if (typeArray.hasValue(R.styleable.SantaEditText_isSlim)) {
+                val isSlim = typeArray.getBoolean(R.styleable.SantaEditText_isSlim, false)
+                if (isSlim) {
                 }
-                if (!isWarning) {
-                    isWarning = true
-                    onWarning.invoke(isWarning)
+            }
+
+            if (typeArray.hasValue(R.styleable.SantaEditText_maxLength)) {
+                val maxLength = typeArray.getInt(R.styleable.SantaEditText_maxLength, -1)
+                if (maxLength != -1) {
+                    setMaxLength(maxLength)
                 }
+            }
+
+            if (typeArray.hasValue(R.styleable.SantaEditText_maxLines)) {
+                val maxLines = typeArray.getInt(R.styleable.SantaEditText_maxLines, -1)
+                if (maxLines != -1) {
+                    setMaxLines(maxLines)
+                }
+            }
+
+            typeArray.recycle()
+
+            when (buttonStyle) {
+                BUTTON_NONE -> rightButton.visibility = View.INVISIBLE
+                BUTTON_ADD -> setAddImage()
+                BUTTON_DELETE -> setDeleteImage()
+            }
+
+            rightButton.setOnClickListener {
+                if (text.isNullOrBlank()) {
+                    return@setOnClickListener
+                }
+                addListener?.invoke(text!!)
+            }
+        }
+
+        fun setButtonClickListener(listener: (String) -> Unit) {
+            addListener = listener
+        }
+
+        fun setAddImage() {
+            rightButton.run {
+                rightButton.visibility = View.VISIBLE
+                setPlusImage(true)
+            }
+            buttonStyle = BUTTON_ADD
+        }
+
+        fun setDeleteImage() {
+            rightButton.run {
+                rightButton.visibility = View.VISIBLE
+                setCancelImage()
+            }
+            buttonStyle = BUTTON_DELETE
+        }
+
+        private fun setMaxLength(maxLength: Int) {
+            editText.filters = arrayOf<InputFilter>(InputFilter.LengthFilter(maxLength))
+        }
+
+        private fun setMaxLines(maxLines: Int) {
+            editText.maxLines = maxLines
+        }
+
+        fun setContentWidth(width: Int) {
+            editText.layoutParams.width = width
+            preview.layoutParams.width = width
+        }
+
+        override fun setOnKeyListener(l: OnKeyListener) {
+            editText.setOnKeyListener(l)
+        }
+
+        override fun addTextChangeListener(textWatcher: TextWatcher) {
+            editText.addTextChangedListener(textWatcher)
+        }
+
+        fun compress(isCompress: Boolean) {
+            if (isCompress) {
+                preview.text = text
+                editText.visibility = View.GONE
             } else {
-                if (isWarning) {
-                    isWarning = false
-                    onWarning.invoke(isWarning)
-                }
+                preview.text = null
+                editText.visibility = View.VISIBLE
             }
         }
 
-        override fun afterTextChanged(s: Editable?) {}
+        fun setSelection(index: Int) {
+            editText.setSelection(index)
+        }
+
+        class SantaEditLimitLengthWatcher(
+            private val nameInput: SantaEditText,
+            private val maxLength: Int,
+            private val onWarning: (isWarning: Boolean) -> Unit,
+        ) : TextWatcher {
+            private var isWarning = false
+            private var preText = ""
+
+            override fun beforeTextChanged(
+                s: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int,
+            ) {
+                preText = s.toString()
+            }
+
+            override fun onTextChanged(
+                s: CharSequence?,
+                start: Int,
+                before: Int,
+                count: Int,
+            ) {
+                if (s.isNullOrEmpty() || s == preText) {
+                    return
+                }
+                if (s.length > maxLength) {
+                    nameInput.run {
+                        text = s.substring(0, maxLength)
+                        setSelection(maxLength)
+                    }
+                    if (!isWarning) {
+                        isWarning = true
+                        onWarning.invoke(isWarning)
+                    }
+                } else {
+                    if (isWarning) {
+                        isWarning = false
+                        onWarning.invoke(isWarning)
+                    }
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        }
     }
-}
--- app/src/main/java/org/sopt/santamanitto/view/SantaSmallButton.kt
+++ app/src/main/java/org/sopt/santamanitto/view/SantaSmallButton.kt
@@ -6,73 +6,82 @@ import androidx.annotation.DrawableRes
 import androidx.appcompat.widget.AppCompatImageButton
 import org.sopt.santamanitto.R
 
-class SantaSmallButton @JvmOverloads constructor(
+class SantaSmallButton
+    @JvmOverloads
+    constructor(
         context: Context,
         attrs: AttributeSet? = null,
-        defStyleAttr: Int = 0
-) : AppCompatImageButton(context, attrs, defStyleAttr) {
-
-    companion object {
-        private const val BUTTON_MINUS = 0
-        private const val BUTTON_PLUS = 1
-        private const val BUTTON_CANCEL = 2
-    }
-
-    private var buttonStyle: Int = 1
-
-    init {
-        scaleType = ScaleType.CENTER_INSIDE
-        //속성 가져오기
-        val typeArray = context.theme.obtainStyledAttributes(
-                attrs,
-                R.styleable.SantaSmallButton,
-                0, 0)
-
-        if (typeArray.hasValue(R.styleable.SantaSmallButton_buttonMode)) {
-            buttonStyle = typeArray.getInt(R.styleable.SantaSmallButton_buttonMode, BUTTON_PLUS)
+        defStyleAttr: Int = 0,
+    ) : AppCompatImageButton(context, attrs, defStyleAttr) {
+        companion object {
+            private const val BUTTON_MINUS = 0
+            private const val BUTTON_MINUS_LIMIT = 1
+            private const val BUTTON_PLUS = 2
+            private const val BUTTON_PLUS_LIMIT = 3
+            private const val BUTTON_CANCEL = 4
         }
 
-        typeArray.recycle()
+        private var buttonStyle: Int = 1
 
-        //이미지 초기화
-        updateButtonImage()
-    }
+        init {
+            scaleType = ScaleType.CENTER_INSIDE
+            // 속성 가져오기
+            val typeArray =
+                context.theme.obtainStyledAttributes(
+                    attrs,
+                    R.styleable.SantaSmallButton,
+                    0,
+                    0,
+                )
 
-    /**
-     * (+) 모양의 이미지로 적용
-     */
-    fun setPlusImage() {
-        setButtonImage(BUTTON_PLUS)
-    }
+            if (typeArray.hasValue(R.styleable.SantaSmallButton_buttonMode)) {
+                buttonStyle = typeArray.getInt(R.styleable.SantaSmallButton_buttonMode, BUTTON_PLUS)
+            }
 
-    /**
-     * (-) 모양의 이미지로 적용
-     */
-    fun setMinusImage() {
-        setButtonImage(BUTTON_MINUS)
-    }
+            typeArray.recycle()
 
-    /**
-     * (X) 모양의 이미지로 적용
-     */
-    fun setCancelImage() {
-        setButtonImage(BUTTON_CANCEL)
-    }
-
-    // 해당 id로 이미지 적용
-    private fun setButtonImage(id: Int) {
-        buttonStyle = id
-        updateButtonImage()
-    }
-
-    // 이미지 업데이트
-    private fun updateButtonImage() {
-        @DrawableRes
-        val resId = when (buttonStyle) {
-            BUTTON_CANCEL -> R.drawable.ic_btn_cancle
-            BUTTON_MINUS -> R.drawable.ic_btn_minus
-            else -> R.drawable.ic_btn_plus
+            // 이미지 초기화
+            updateButtonImage()
         }
-        setBackgroundResource(resId)
+
+        /**
+         * (+) 모양의 이미지로 적용
+         */
+        fun setPlusImage(isAvailable: Boolean) {
+            setButtonImage(if (isAvailable) BUTTON_PLUS else BUTTON_PLUS_LIMIT)
+        }
+
+        /**
+         * (-) 모양의 이미지로 적용
+         */
+        fun setMinusImage(isAvailable: Boolean) {
+            setButtonImage(if (isAvailable) BUTTON_MINUS else BUTTON_MINUS_LIMIT)
+        }
+
+        /**
+         * (X) 모양의 이미지로 적용
+         */
+        fun setCancelImage() {
+            setButtonImage(BUTTON_CANCEL)
+        }
+
+        // 해당 id로 이미지 적용
+        private fun setButtonImage(id: Int) {
+            buttonStyle = id
+            updateButtonImage()
+        }
+
+        // 이미지 업데이트
+        private fun updateButtonImage() {
+            @DrawableRes
+            val resId =
+                when (buttonStyle) {
+                    BUTTON_MINUS -> R.drawable.ic_minus_dark_gray
+                    BUTTON_MINUS_LIMIT -> R.drawable.ic_minus_light_gray
+                    BUTTON_PLUS -> R.drawable.ic_plus_dark_gray
+                    BUTTON_PLUS_LIMIT -> R.drawable.ic_plus_light_gray
+                    else -> R.drawable.ic_btn_cancle
+                }
+            setBackgroundResource(resId)
+        }
     }
-}
