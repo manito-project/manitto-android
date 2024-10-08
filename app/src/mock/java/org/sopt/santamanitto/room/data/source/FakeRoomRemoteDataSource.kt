a774397815bf27f117c0883d7d95d4125b4ac5bc
HaeTi <t9h9@naver.com>
2024-10-09 02:39:24 +0900

    [CHORE] 가짜 데이터 ID 자료형 변경사항 대응

--- app/src/mock/java/org/sopt/santamanitto/room/data/source/FakeRoomRemoteDataSource.kt
+++ app/src/mock/java/org/sopt/santamanitto/room/data/source/FakeRoomRemoteDataSource.kt
@@ -3,43 +3,48 @@ package org.sopt.santamanitto.room.data.source
 import org.sopt.santamanitto.room.data.MissionContentModel
 import org.sopt.santamanitto.room.data.PersonalRoomModel
 
-class FakeRoomRemoteDataSource: RoomDataSource {
+class FakeRoomRemoteDataSource : RoomDataSource {
 
     private val fakePersonalRoomInfos = HashMap<Int, PersonalRoomModel>().apply {
-        put(1, PersonalRoomModel(
-                1,
-                2,
+        put(
+            1, PersonalRoomModel(
+                "1",
+                "2",
                 MissionContentModel("fake my mission"),
                 MissionContentModel("fake mission to me")
+            )
         )
-        )
-        put(2, PersonalRoomModel(
-                1,
-                3,
+        put(
+            2, PersonalRoomModel(
+                "1",
+                "3",
                 MissionContentModel("fake my mission"),
                 MissionContentModel("fake mission to me")
+            )
         )
-        )
-        put(3, PersonalRoomModel(
-                1,
-                4,
+        put(
+            3, PersonalRoomModel(
+                "1",
+                "4",
                 MissionContentModel("fake my mission"),
                 MissionContentModel("fake mission to me")
+            )
         )
-        )
-        put(4, PersonalRoomModel(
-                1,
-                5,
+        put(
+            4, PersonalRoomModel(
+                "1",
+                "5",
                 MissionContentModel("fake my mission"),
                 MissionContentModel("fake mission to me")
+            )
         )
-        )
-        put(5, PersonalRoomModel(
-                1,
-                6,
+        put(
+            5, PersonalRoomModel(
+                "1",
+                "6",
                 MissionContentModel("fake my mission"),
                 MissionContentModel("fake mission to me")
-        )
+            )
         )
     }
 
