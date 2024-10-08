2acc2440b9b0a3f156c1e48735277e69c1334146
HaeTi <t9h9@naver.com>
2024-10-09 02:28:04 +0900

    [CHORE] 레거시 데이터 모델 삭제

--- app/src/main/java/org/sopt/santamanitto/user/network/UserCheckModel.kt
+++
@@ -1,8 +0,0 @@
-package org.sopt.santamanitto.user.network
-
-import org.sopt.santamanitto.user.data.UserLoginModel
-
-data class UserCheckModel(
-    val user: UserLoginModel,
-    val accessToken: String
-)
