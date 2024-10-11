875d594e35ecb34355f2f0bd9ce2721bb0df7722
HaeTi <t9h9@naver.com>
2024-10-11 23:07:19 +0900

    [REFACTOR] 가짜 로그인/회원가입 로직 수정

--- app/src/mock/java/org/sopt/santamanitto/user/data/controller/FakeUserController.kt
+++ app/src/mock/java/org/sopt/santamanitto/user/data/controller/FakeUserController.kt
@@ -1,35 +1,38 @@
 package org.sopt.santamanitto.user.data.controller
 
-import android.util.Log
-import kotlinx.coroutines.*
-import org.sopt.santamanitto.user.data.UserLoginModel
+import kotlinx.coroutines.delay
+import org.sopt.santamanitto.auth.data.response.SignInResponseModel
+import org.sopt.santamanitto.auth.data.response.SignUpResponseModel
 
 class FakeUserController : UserController {
 
-    private val fakeLoginUser = UserLoginModel(
-        "fakeUser",
-        "f0a1k2e3u4e5s6e7r8",
-        1,
-        "fake1access2token3"
+    private val fakeLoginUser = SignInResponseModel(
+        accessToken = "f0a1k2e3u4e5s6e7r8",
+        id = "1"
     )
 
-    override fun login(serialNumber: String, callback: UserController.LoginCallback) {
-//        callback.onLoginSuccess(fakeLoginUser)
-        callback.onLoginFailed(false)
+    private val fakeSignUpUser = SignUpResponseModel(
+        accessToken = "fake1access2token3",
+        id = "1"
+    )
+
+    override suspend fun login(serialNumber: String): Result<SignInResponseModel> {
+        return try {
+            Result.success(fakeLoginUser)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
     }
 
-    override fun createAccount(
+    override suspend fun createAccount(
         userName: String,
-        serialNumber: String,
-        callback: UserController.CreateAccountCallback
-    ) {
-        GlobalScope.launch {
-            Log.d(javaClass.simpleName, "createAccount(): request ...")
+        serialNumber: String
+    ): Result<SignUpResponseModel> {
+        return try {
             delay(2000)
-            Log.d(javaClass.simpleName, "createAccount(): response!")
-            withContext(Dispatchers.Main) {
-                callback.onCreateAccountSuccess(fakeLoginUser)
-            }
+            Result.success(fakeSignUpUser)
+        } catch (e: Exception) {
+            Result.failure(e)
         }
     }
 }
