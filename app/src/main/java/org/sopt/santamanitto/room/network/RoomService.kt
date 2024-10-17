5bdd8bcb60fe716e5f7d89bd37b4325bec473a2f
HaeTi <t9h9@naver.com>
2024-10-17 23:50:00 +0900

    [FEAT] get rooms API 서비스 구현

--- app/src/main/java/org/sopt/santamanitto/room/network/RoomService.kt
+++ app/src/main/java/org/sopt/santamanitto/room/network/RoomService.kt
@@ -2,15 +2,16 @@ package org.sopt.santamanitto.room.network
 
 import org.sopt.santamanitto.network.Response
 import org.sopt.santamanitto.network.SimpleResponse
-import org.sopt.santamanitto.room.create.network.CreateRoomRequestModel
 import org.sopt.santamanitto.room.create.network.CreateRoomModel
+import org.sopt.santamanitto.room.create.network.CreateRoomRequestModel
 import org.sopt.santamanitto.room.create.network.ModifyRoomRequestModel
 import org.sopt.santamanitto.room.data.PersonalRoomModel
-import org.sopt.santamanitto.room.join.network.JoinRoomRequestModel
+import org.sopt.santamanitto.room.data.TempMyManittoModel
 import org.sopt.santamanitto.room.join.network.JoinRoomModel
-import org.sopt.santamanitto.room.manittoroom.network.MatchingRequestModel
+import org.sopt.santamanitto.room.join.network.JoinRoomRequestModel
 import org.sopt.santamanitto.room.manittoroom.network.ManittoRoomModel
 import org.sopt.santamanitto.room.manittoroom.network.MatchedMissionsModel
+import org.sopt.santamanitto.room.manittoroom.network.MatchingRequestModel
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.DELETE
@@ -20,10 +21,12 @@ import retrofit2.http.PUT
 import retrofit2.http.Path
 
 interface RoomService {
+    @GET("rooms")
+    suspend fun getRooms(): Response<List<TempMyManittoModel>>
 
     @GET("rooms/{roomId}/my")
     fun getRoomPersonalInfo(
-        @Path("roomId") roomId: Int
+        @Path("roomId") roomId: String
     ): Call<Response<PersonalRoomModel>>
 
     @POST("rooms")
@@ -33,7 +36,7 @@ interface RoomService {
 
     @PUT("rooms/{roomId}")
     fun modifyRoom(
-        @Path("roomId") roomId: Int,
+        @Path("roomId") roomId: String,
         @Body request: ModifyRoomRequestModel
     ): Call<Response<SimpleResponse>>
 
@@ -44,7 +47,7 @@ interface RoomService {
 
     @GET("rooms/{roomId}")
     fun getManittoRoomData(
-        @Path("roomId") roomId: Int
+        @Path("roomId") roomId: String
     ): Call<Response<ManittoRoomModel>>
 
     @POST("rooms/match")
@@ -59,6 +62,6 @@ interface RoomService {
 
     @DELETE("rooms/{roomId}/history")
     fun removeHistory(
-        @Path("roomId") roomId: Int
+        @Path("roomId") roomId: String
     ): Call<SimpleResponse>
 }
