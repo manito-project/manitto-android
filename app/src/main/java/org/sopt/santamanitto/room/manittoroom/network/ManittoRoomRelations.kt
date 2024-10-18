3a37058b2fd525947969cb5a87fec0d5b9a8eef1
Sangho Kim <marchbreeze03@gmail.com>
2024-10-18 14:31:57 +0900

    [FIX/#164] 방 상세조회 dto 수정

--- app/src/main/java/org/sopt/santamanitto/room/manittoroom/network/ManittoRoomRelations.kt
+++
@@ -1,8 +0,0 @@
-package org.sopt.santamanitto.room.manittoroom.network
-
-import com.google.gson.annotations.SerializedName
-
-data class ManittoRoomRelations(
-    @SerializedName("SantaUserId") val santaUserId: String,
-    @SerializedName("ManittoUserId") val manittoUserId: String
-)
