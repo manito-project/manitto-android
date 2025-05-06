a5ec1fa278c43ee1a398b1474e5ba06f9708e07f
haeti-dev <t9h9@soongsil.ac.kr>
2025-05-06 23:08:15 +0900

    [FEAT/#218] 홈 화면에 광고 배너 추가

--- app/src/main/java/org/sopt/santamanitto/main/MainActivity.kt
+++ app/src/main/java/org/sopt/santamanitto/main/MainActivity.kt
@@ -3,6 +3,7 @@ package org.sopt.santamanitto.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil.setContentView
+import com.google.android.gms.ads.MobileAds
 import dagger.hilt.android.AndroidEntryPoint
 import org.sopt.santamanitto.R
 import org.sopt.santamanitto.databinding.ActivityMainBinding
@@ -11,6 +12,13 @@ import org.sopt.santamanitto.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        initializeAds()
+
         setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
+
+    private fun initializeAds() {
+        MobileAds.initialize(this)
     }
 }
