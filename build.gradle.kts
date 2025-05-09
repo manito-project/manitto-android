6139bc637aedf449bc893e3abff3f92990df2a65
Sangho Kim <marchbreeze03@gmail.com>
2025-05-09 19:04:56 +0900

    [REFACTOR/#219] setting gradle 파일 kts 전환

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.kapt) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.androidx.navigation.safeargs) apply false
+    alias(libs.plugins.google.services) apply false
+    alias(libs.plugins.firebase.crashlytics) apply false
+}
