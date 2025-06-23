4e97f949c538daa76cf88dac8fd23b7464f26c42
Sangho Kim <marchbreeze03@gmail.com>
2025-06-23 23:48:41 +0900

    [FEAT/#227] 채팅목록뷰 어댑터 추가 및 리사이클러뷰 연결

--- app/src/main/java/org/sopt/santamanitto/chat/overview/ChatOverviewActivity.kt
+++ app/src/main/java/org/sopt/santamanitto/chat/overview/ChatOverviewActivity.kt
@@ -1,16 +1,49 @@
 package org.sopt.santamanitto.chat.overview
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil.setContentView
 import dagger.hilt.android.AndroidEntryPoint
-import org.sopt.santamanitto.R
 import org.sopt.santamanitto.databinding.ActivityChatOverviewBinding
 
 @AndroidEntryPoint
 class ChatOverviewActivity : AppCompatActivity() {
+    private val viewModel by viewModels<ChatOverviewViewModel>()
+
+    private lateinit var binding: ActivityChatOverviewBinding
+
+    private var _adapter: ChatOverviewAdapter? = null
+    val adapter
+        get() = requireNotNull(_adapter)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView<ActivityChatOverviewBinding>(this, R.layout.activity_chat_overview)
+        binding = ActivityChatOverviewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initAdapter()
+        // TODO: 배너광고 추가
+    }
+
+    private fun initAdapter() {
+        _adapter = ChatOverviewAdapter(
+            itemClick = ::initItemClickListener,
+            itemLongClick = ::initItemLongClickListener,
+        )
+        binding.recyclerviewChatOverview.adapter = adapter
+    }
+
+    private fun initItemClickListener(roomId: String) {
+
+    }
+
+    private fun initItemLongClickListener(roomId: String) {
+        // TODO: 삭제 기능 추가
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        _adapter = null
     }
 }
---
+++ app/src/main/java/org/sopt/santamanitto/chat/overview/ChatOverviewViewModel.kt
@@ -0,0 +1,10 @@
+package org.sopt.santamanitto.chat.overview
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ChatOverviewViewModel @Inject constructor() : ViewModel() {
+
+}
